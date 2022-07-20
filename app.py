47d15178f803c3a49f3470b8b94fa8433479f4b9
chenrohua24 <z7707092003@gmail.com>
2022-07-20 21:40:17 +0800

    chore: add process real estate logic

---
+++ app.py
@@ -0,0 +1,17 @@
+import logging
+
+from real_.process import process_real_estate
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)10.19s [%(levelname)s] %(message)s",
+    handlers=[logging.FileHandler('debug.log'), logging.StreamHandler()],
+)
+
+
+def main():
+    process_real_estate()
+
+
+if __name__ == '__main__':
+    main()
---
+++ real_/process.py
@@ -0,0 +1,12 @@
+import logging
+
+logger = logging.getLogger()
+
+
+def process_real_estate():
+    logger.info(f'process_real_estate')
+    # fetch real estate
+    # save
+    # upload local or s3
+    # load into database
+    pass
