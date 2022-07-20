b444bfe88cf53ff80634f5b72496f830683c6cee
chenrohua24 <z7707092003@gmail.com>
2022-07-20 22:34:16 +0800

    chore: update process logic

--- real_/process.py
+++ real_/process.py
@@ -9,7 +9,8 @@ logger = logging.getLogger()
 def process_real_estate():
     year, season = sys.argv[1:3]
     real_estate = fetch_real_estate(year, season)
-    # save
+    # extract all
+    # parse
     # upload local or s3
     # load into database
     logger.info(f'process_real_estate, {real_estate.status_code}')
