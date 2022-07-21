fc4ae9403d0f0c8d36def399b5def66fe162abe2
chenrohua24 <z7707092003@gmail.com>
2022-07-21 23:09:31 +0800

    feat: modify url params and return value of fetch real estate

--- real_/fetch.py
+++ real_/fetch.py
@@ -1,35 +1,30 @@
 import logging
-from pathlib import Path
 
 import requests
-
-from real_.config import STORE_ROOT_DIR
 from real_.utils import constants
-from real_.utils.file import extract_to
 
 logger = logging.getLogger()
 
 
 def fetch_real_estate(year, season) -> None:
+    logger.info('step: fetch_real_estate')
+
     year = int(year)
     if year > 1000:
         year -= 1911
 
     params = {
         'season': f'{year}S{season}',
-        'type': 'zip',
-        'fileName': 'lvr_landcsv.zip'
+        'fileName': 'F_lvr_land_B.csv',
     }
 
     try:
-        resp = requests.get(
+        return requests.get(
             url='https://plvr.land.moi.gov.tw//DownloadSeason',
             params=params,
             headers=constants.HEADERS
         )
-        extract_to(
-            folderpath=Path(STORE_ROOT_DIR).joinpath(f'{year}-{season}'),
-            content=resp.content,
-        )
     except Exception as e:
         logger.warning(f'fetch error: {repr(e)}')
+
+        return None
