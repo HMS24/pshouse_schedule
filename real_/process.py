4be1dbd24fcb6917e0328e30f79e047141225569
chenrohua24 <z7707092003@gmail.com>
2022-07-22 13:10:53 +0800

    refactor: rename

--- real_/process.py
+++ real_/process.py
@@ -13,15 +13,15 @@ def process_real_estate():
     logger.info('step: process_real_estate')
 
     year, season = sys.argv[1:3]
-    real_estate_info = fetch_real_estate(year, season)
+    real_estate_content = fetch_real_estate(year, season)
 
-    if real_estate_info is None:
+    if real_estate_content is None:
         return
 
     save_to_storage(
         dir_name=f'{year}S{season}',
         filepath=Path(config.STORAGE_ROOT_DIR).joinpath('F_lvr_land_B.csv'),
-        content=real_estate_info
+        content=real_estate_content
     )
 
     # parse
