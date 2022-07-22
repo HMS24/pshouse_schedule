923ba61fdffc81cd9162185b50ad74b039dd90d3
chenrohua24 <z7707092003@gmail.com>
2022-07-22 23:03:17 +0800

    feat: parse real estate info

--- real_/parse.py
+++ real_/parse.py
@@ -1,7 +1,121 @@
+import logging
+from datetime import datetime
+
+from pydantic import BaseModel, ValidationError
+
+
+logger = logging.getLogger()
+
+zh_en_map = {
+    '鄉鎮市區': 'district',
+    '交易標的': 'object_of_transaction',
+    '土地位置建物門牌': 'location',
+    '土地移轉總面積平方公尺': 'land_total_area',
+    '交易年月日': 'transaction_date',
+    '移轉層次': 'level',
+    '總樓層數': 'total_floor_numbers',
+    '建物型態': 'building_state',
+    '主要用途': 'main_use',
+    '建物移轉總面積平方公尺': 'building_total_area',
+    '建物現況格局-房': 'room',
+    '建物現況格局-廳': 'restaurant_and_living_room',
+    '建物現況格局-衛': 'bathroom',
+    '總價元': 'price',
+    '單價元平方公尺': 'unit_price',
+    '車位類別': 'parking_sapce_type',
+    '車位移轉總面積平方公尺': 'parking_sapce_total_area',
+    '車位總價元': 'parking_sapce_price',
+    '備註': 'note',
+    '建案名稱': 'build_case',
+    '棟及號': 'buildings',
+}
+
+
+class RealEstateInfo(BaseModel):
+    district: str = None
+    object_of_transaction: str = None
+    location: str = None
+    transaction_date: datetime = None
+    level: str = None
+    total_floor_numbers: str = None
+    main_use: str = None
+    building_state: str = None
+    room: str = None
+    restaurant_and_living_room: str = None
+    bathroom: str = None
+    land_total_area: float = 0.00
+    building_total_area: float = 0.00
+    parking_sapce_total_area: float = 0.00
+    price: int = 0
+    unit_price: int = 0
+    parking_sapce_type: str = None
+    parking_sapce_price: int = 0
+    note: str = None
+    build_case: str = None
+    buildings: str = None
+
+
+def translate_column_names(columns, mapper):
+    return [mapper[col] for col in columns]
+
+
+def convert_roc_to_ad_date(date_str):
+    roc_year = date_str[:3]
+    date_str = str(int(roc_year) + 1911) + date_str[3:]
+
+    return datetime.strptime(date_str, '%Y%m%d')
+
+
 def parse_real_estate_info(df):
-    # copy df
-    # remove english description row(second)
+    logger.info('step: parse_real_estate_info')
+
+    df = df.copy()
+
+    # get needed columns
+    df = df[zh_en_map.keys()]
+
     # translate columns name
+    df.columns = translate_column_names(df.columns, zh_en_map)
+
+    # remove english description row(usually second row)
+    en_column_name_index = df[df['buildings'] == 'buildings'].index
+    df = df.drop(en_column_name_index)
+
     # parse ROC date
+    df['transaction_date'] = (
+        df['transaction_date']
+        .apply(convert_roc_to_ad_date)
+    )
+
+    # fill NaN
+    for col in zh_en_map.values():
+        if col in ['transaction_date']:
+            continue
+        elif col in [
+            'land_total_area',
+            'building_total_area',
+            'parking_sapce_total_area',
+            'price',
+            'unit_price',
+            'parking_sapce_price',
+        ]:
+            df[col] = df[col].fillna(0)
+        else:
+            df[col] = df[col].fillna('')
+
     # cast type
-    pass
+    try:
+        # why not use comprehension? because easily debug when using row
+        results = []
+        for row in df.to_dict('records'):
+            results.append(RealEstateInfo(**row).dict())
+
+        return results
+    except ValidationError as e:
+        logger.warning(
+            f'real estate parse failed: {repr(e)}    ',
+            f'district: {row["district"]}, ',
+            f'transaction_date: {row["transaction_date"]}, ',
+            f'build_case: {row["build_case"]}, ',
+            f'buildings: {row["buildings"]}, ',
+        )
--- real_/process.py
+++ real_/process.py
@@ -2,15 +2,18 @@ import sys
 import logging
 from pathlib import Path
 
+import pandas as pd
+
 from real_ import config
 from real_.fetch import fetch_real_estate
 from real_.storage import save_to_storage
+from real_.parse import parse_real_estate_info
 
 logger = logging.getLogger()
 
 
 def process_real_estate():
-    logger.info('step: process_real_estate')
+    logger.info('start process_real_estate')
 
     year, season = sys.argv[1:3]
     real_estate_content = fetch_real_estate(year, season)
@@ -18,11 +21,17 @@ def process_real_estate():
     if real_estate_content is None:
         return
 
+    # save as a local file then upload storage
+    filepath = Path(config.STORAGE_ROOT_DIR).joinpath('F_lvr_land_B.csv')
     save_to_storage(
         dir_name=f'{year}S{season}',
-        filepath=Path(config.STORAGE_ROOT_DIR).joinpath('F_lvr_land_B.csv'),
+        filepath=filepath,
         content=real_estate_content
     )
 
-    # parse
+    # encoding 'utf-8-sig' for escaping UTF16_BOM
+    with open(filepath, 'r', encoding='utf-8-sig') as f:
+        df = pd.read_csv(f)
+        real_estate_info = parse_real_estate_info(df)
+
     # load into database
