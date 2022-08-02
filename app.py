5652e59ef1e0ec8955f726b2eb3dcd241ea2e079
chenrohua24 <z7707092003@gmail.com>
2022-08-02 14:18:34 +0800

    feat: schedule process at 1, 11, 21 of every month

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-import sys
+import time
 import logging
 
+from apscheduler.schedulers.background import BackgroundScheduler
 from presale.process import process_actual_price_registration
 
 logging.basicConfig(
@@ -11,8 +12,22 @@ logging.basicConfig(
 
 
 def main():
-    year, season = sys.argv[1:3]
-    process_actual_price_registration(year, season)
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(
+        id="crawl_presale_actual_price_registration",
+        func=process_actual_price_registration,
+        trigger="cron",
+        year="*",
+        month="*",
+        day="1, 11, 21",
+        hour="10",
+        minute="0",
+    )
+    scheduler.start()
+
+    # This is here to simulate application activity (which keeps the main thread alive).
+    while True:
+        time.sleep(2)
 
 
 if __name__ == "__main__":
