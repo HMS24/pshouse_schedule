019e0e72bd83a5e59c1b6f84b22e28cc326ab431
chenrohua24 <z7707092003@gmail.com>
2022-08-02 15:27:13 +0800

    feat: fetch by year season

--- presale/crawlers/process.py
+++ presale/crawlers/process.py
@@ -2,7 +2,10 @@ import json
 import logging
 import datetime
 
-from presale.crawlers.fetch import fetch_actual_price_registration
+from presale.crawlers.fetch import (
+    fetch_actual_price_registration,
+    fetch_actual_price_registration_by_year_season,
+)
 from presale.crawlers.parse import (
     parse_actual_price_registration,
     parse_problem_actual_price_registration,
@@ -34,6 +37,34 @@ def process_actual_price_registration():
     need_checked = parse_problem_actual_price_registration(need_checked)
     save_to_storage(
         dirname=f"{today}",
+        filename="F_lvr_land_B_need_checked.json",
+        content=json.dumps(
+            obj=need_checked,
+            indent=4,
+            ensure_ascii=False,
+        ).encode("utf-8"),
+    )
+
+
+def process_actual_price_registration_by_year_season(year, season):
+    logger.info("start process_actual_price_registration_by_year_season")
+
+    content = fetch_actual_price_registration_by_year_season(year, season)
+
+    if content is None:
+        return
+
+    save_to_storage(
+        dirname=f"{year}_{season}",
+        filename="F_lvr_land_B.csv",
+        content=content,
+    )
+
+    _, need_checked = parse_actual_price_registration(content)
+    need_checked = parse_problem_actual_price_registration(need_checked)
+
+    save_to_storage(
+        dirname=f"{year}_{season}",
         filename="F_lvr_land_B_need_checked.json",
         content=json.dumps(
             obj=need_checked,
