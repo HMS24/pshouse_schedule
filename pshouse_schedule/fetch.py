ea63719da729037595ddd34002eb02affd4d6125
chenrohua24 <z7707092003@gmail.com>
2022-08-31 23:55:23 +0800

    chore: rename package

--- pshouse_schedule/fetch.py
+++ pshouse_schedule/fetch.py
@@ -2,13 +2,13 @@ import logging
 import datetime
 
 import requests
-from presale.utils import constants
+import pshouse_schedule.utils as utils
 
 logger = logging.getLogger()
 
 
-def fetch_actual_price_registration():
-    logger.info("   step: fetch_actual_price_registration")
+def fetch_deals():
+    logger.info("   step: fetch_deals")
 
     params = {
         "fileName": "f_lvr_land_b.csv",
@@ -18,7 +18,7 @@ def fetch_actual_price_registration():
         resp = requests.get(
             url="https://plvr.land.moi.gov.tw//Download",
             params=params,
-            headers=constants.HEADERS,
+            headers=utils.HEADERS,
         )
 
         # 當找不到該季或不正確的 fileName 時，會回傳 "系統發生錯誤，請洽系統管理人員" 的 html
@@ -26,7 +26,7 @@ def fetch_actual_price_registration():
         if int(resp.headers["Content-Length"]) == 541:
             today = str(datetime.datetime.now().date())
             raise Exception(
-                f"current date {str(datetime.datetime.now().date())} dataset has not been updated")
+                f"current date {today} dataset has not been updated")
 
         return resp.content
     except Exception as e:
