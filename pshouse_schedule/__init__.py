33738dbbf4645e03d7a68f400ae0c80b7c294e1f
chenrohua24 <z7707092003@gmail.com>
2022-09-05 23:11:49 +0800

    style: import order

--- pshouse_schedule/__init__.py
+++ pshouse_schedule/__init__.py
@@ -1,6 +1,7 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+
 import pshouse_schedule.config as config
 from pshouse_schedule.db.database import Database
-from apscheduler.schedulers.background import BackgroundScheduler
 
 db = Database(config.DATABASE_URI)
 scheduler = BackgroundScheduler()
