e99178084e38a5660294795409d7deb979a11829
chenrohua24 <z7707092003@gmail.com>
2022-09-11 23:41:45 +0800

    chore: finish process test

--- tests/mock.py
+++ tests/mock.py
@@ -1,29 +1,6 @@
+from pathlib import Path
 from pandas import Timestamp
 from dataclasses import dataclass
-
-
-@dataclass
-class MockResponse:
-    text: str = ""
-    status_code: int = 200
-
-
-def mock_request_deals(url, params, headers):
-    with open("./tests/data/F_lvr_land_B.csv", "r") as f:
-        return MockResponse(text=f.read())
-
-
-def mock_request_deals_failed(url, params, headers,):
-    text = """
-        \r\n \r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n \r\n<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">\r\n\r\n<html>\r\n<head>\r\n<title>施工中..</title>\r\n</head>\r\n<body>\r\n\t<table Border=\'0\' width="380" height="200" border="0" cellpadding="0"\r\n\t\tcellspacing="0" valign="top" align="center" background="">\r\n\t\t<tr align=left>\r\n\t\t\t<td align="left">檔案不存在</td>\r\n\t\t</tr>\r\n\t\t<tr>\r\n\t\t\t<td align=right valign="bottom">\r\n\t\t\t\t<img border="0"\tsrc="images/404.png">\r\n\t\t\t</td>\r\n\t\t</tr>\r\n\t\t<tr height=50 align=center>\r\n\t\t\t<td colspan=2 align=center><font size=2 color="red">&nbsp;\r\n\t\t\t</td>\r\n\t\t</tr>\r\n\t</table>\r\n</body>\r\n</html>\r\n
-    """
-    return MockResponse(text=text, status_code=404)
-
-
-def mock_request_history_deals_list(url):
-    with open("./tests/data/history.html", "r", encoding="utf-8") as f:
-        return MockResponse(text=f.read())
-
 
 EXPECTED_DEALS = [
     {
@@ -73,7 +50,6 @@ EXPECTED_DEALS = [
         'buildings': 'C棟1號',
     },
 ]
-
 EXPECTED_NEED_CHECKED_DEALS = [
     {
         'location': '新北市五股區成泰路三段',
@@ -100,7 +76,6 @@ EXPECTED_NEED_CHECKED_DEALS = [
         'error_message': "ValidationError(model='Deal', errors=[{'loc': ('room',), 'msg': 'There are too many rooms. 999 rooms?', 'type': 'assertion_error'}])",
     },
 ]
-
 EXPECTED_NEED_CHECKED_DEALS_PARSED = [
     {
         '土地位置建物門牌': '新北市五股區成泰路三段',
@@ -127,3 +102,49 @@ EXPECTED_NEED_CHECKED_DEALS_PARSED = [
         'error_message': "ValidationError(model='Deal', errors=[{'loc': ('room',), 'msg': 'There are too many rooms. 999 rooms?', 'type': 'assertion_error'}])",
     },
 ]
+
+
+@dataclass
+class MockResponse:
+    text: str = ""
+    status_code: int = 200
+
+
+@dataclass
+class MockConfig:
+    STORAGE_ROOT_DIR: str = "./tests/data/results"
+    STORAGE_BUCKET_NAME: str = "this_is_storage_bucket_name"
+    STORAGE_TYPE: str = "LOCAL"
+    STORAGE_KEY: str = "./tests/data/results"
+    STORAGE_SECRET: str = "secret"
+
+
+MOCK_RESOURCES_FOLDER = Path(MockConfig.STORAGE_ROOT_DIR)
+MOCK_CHECK_FOLDER = Path(MockConfig.STORAGE_ROOT_DIR).joinpath("check")
+
+
+def mock_request_deals(url, params, headers):
+    with open("./tests/data/F_lvr_land_B.csv", "r") as f:
+        return MockResponse(text=f.read())
+
+
+def mock_request_deals_failed(url, params, headers,):
+    text = """\r\n \r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n \r\n<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">\r\n\r\n<html>\r\n<head>\r\n<title>施工中..</title>\r\n</head>\r\n<body>\r\n\t<table Border=\'0\' width="380" height="200" border="0" cellpadding="0"\r\n\t\tcellspacing="0" valign="top" align="center" background="">\r\n\t\t<tr align=left>\r\n\t\t\t<td align="left">檔案不存在</td>\r\n\t\t</tr>\r\n\t\t<tr>\r\n\t\t\t<td align=right valign="bottom">\r\n\t\t\t\t<img border="0"\tsrc="images/404.png">\r\n\t\t\t</td>\r\n\t\t</tr>\r\n\t\t<tr height=50 align=center>\r\n\t\t\t<td colspan=2 align=center><font size=2 color="red">&nbsp;\r\n\t\t\t</td>\r\n\t\t</tr>\r\n\t</table>\r\n</body>\r\n</html>\r\n"""
+    return MockResponse(text=text, status_code=404)
+
+
+def mock_request_history_deals_list(url):
+    with open("./tests/data/history.html", "r", encoding="utf-8") as f:
+        return MockResponse(text=f.read())
+
+
+def mock_fetch_deals():
+    return """鄉鎮市區,交易標的,土地位置建物門牌,土地移轉總面積平方公尺,都市土地使用分區,非都市土地使用分區,非都市土地使用編定,交易年月日,交易筆棟數,移轉層次,總樓層數,建物型態,主要用途,主要建材,建築完成年月,建物移轉總面積平方公尺,建物現況格局-房,建物現況格局-廳,建物現況格局-衛,建物現況格局-隔間,有無管理組織,總價元,單價元平方公尺,車位類別,車位移轉總面積平方公尺,車位總價元,備註,編號,建案名稱,棟及號\nThe villages and towns urban district,transaction sign,land sector position building sector house number plate,land shifting total area square meter,the use zoning or compiles and checks,the non-metropolis land use district,non-metropolis land use,transaction year month and day,transaction pen number,shifting level,total floor number,building state,main use,main building materials,construction to complete the years,building shifting total area,Building present situation pattern - room,building present situation pattern - hall,building present situation pattern - health,building present situation pattern - compartmented,Whether there is manages the organization,total price NTD,the unit price (NTD / square meter),the berth category,berth shifting total area square meter,the berth total price NTD,the note,serial number,build case,buildings\n泰山區,,新北市泰山區坡雅路信華三街口,,住,,,1110713,土地4建物1車位1,十三層,15,住宅大樓(11層含以上有電梯),住家用,鋼筋混凝土造,,93.11,2,2,1,有,無,12500000,157165,坡道平面,28.21,2300000,,RPTOMLNKPHHGFBF28CB,朗朗城心,A棟10-13F號\n土城區,房地(土地+建物)+車位,新北市土城區金城路二段,11.78,商,,,1110711,土地1建物1車位1,十層,13,住宅大樓(11層含以上有電梯),住家用,鋼筋混凝土造,,104.36,1,1,1,有,無,14910000,166351,坡道平面,30.36,2600000,,RPUNMLNKPHHGFAF18CB,有富富玉,C棟1號\n五股區,房地(土地+建物)+車位,新北市五股區成泰路三段,16.62,住,,,1110711,土地1建物1車位1,五層,15,住宅大樓(11層含以上有電梯),住家用,鋼筋混凝土造,,123.73,999,1,1,有,無,12470000,113787,坡道平面,28.64,1650000,,RPOOMLPKPHHGFBF97CB,佳陞景漾,A棟A5-5F號\n"""
+
+
+def mock_parse_deals_info(raw):
+    return (EXPECTED_DEALS, EXPECTED_NEED_CHECKED_DEALS)
+
+
+def mock_parse_incorrect_deals_info(data):
+    return EXPECTED_NEED_CHECKED_DEALS_PARSED
---
+++ tests/test_processes.py
@@ -0,0 +1,69 @@
+import json
+import shutil
+from datetime import datetime
+from unittest import mock
+
+from tests.mock import (
+    MockConfig,
+
+    MOCK_RESOURCES_FOLDER,
+    MOCK_CHECK_FOLDER,
+    EXPECTED_NEED_CHECKED_DEALS_PARSED,
+
+    mock_fetch_deals,
+    mock_parse_deals_info,
+    mock_parse_incorrect_deals_info,
+)
+from pshouse_schedule.processes import crawl_deals
+
+
+@mock.patch("pshouse_schedule.processes.RESOURCES_FOLDER", MOCK_RESOURCES_FOLDER)
+@mock.patch("pshouse_schedule.processes.CHECK_FOLDER", MOCK_CHECK_FOLDER)
+@mock.patch("pshouse_schedule.processes.config", MockConfig)
+@mock.patch("pshouse_schedule.storage.config", MockConfig)
+@mock.patch("pshouse_schedule.processes.fetch_deals", mock_fetch_deals)
+@mock.patch("pshouse_schedule.processes.parse_deals_info", mock_parse_deals_info)
+@mock.patch("pshouse_schedule.processes.load_into_database")
+@mock.patch("pshouse_schedule.processes.parse_incorrect_deals_info", mock_parse_incorrect_deals_info)
+def test_crawl_deals(load_into_database_mocker):
+    # case: success
+    _set_up()
+
+    crawl_deals()
+
+    today = datetime.now().strftime("%Y%m%d")
+    target_filename = MOCK_RESOURCES_FOLDER.joinpath(
+        MockConfig.STORAGE_BUCKET_NAME,
+        f"{today}_F_lvr_land_B.csv",
+    )
+    assert target_filename.exists()
+
+    with open(target_filename, "r", encoding="utf-8-sig") as f:
+        saved_data = f.read()
+    assert saved_data == mock_fetch_deals()
+
+    check_filename = MOCK_RESOURCES_FOLDER.joinpath(
+        "check",
+        f"{today}_F_lvr_land_B.json",
+    )
+    assert check_filename.exists()
+
+    with open(check_filename, "r", encoding="utf-8-sig") as f:
+        saved_data = json.loads(f.read())
+    assert saved_data == EXPECTED_NEED_CHECKED_DEALS_PARSED
+
+    _tear_down()
+
+
+def _set_up():
+    try:
+        shutil.rmtree(MOCK_RESOURCES_FOLDER)
+    except FileNotFoundError:
+        pass
+
+    MOCK_RESOURCES_FOLDER.mkdir(parents=True, exist_ok=True)
+    MOCK_CHECK_FOLDER.mkdir(parents=True, exist_ok=True)
+
+
+def _tear_down():
+    shutil.rmtree(MOCK_RESOURCES_FOLDER)
