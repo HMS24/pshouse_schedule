d1c13effbf8921e36aa4a28662765eb352faf77a
chenrohua24 <z7707092003@gmail.com>
2022-09-14 21:44:09 +0800

    chore: add create history deals in run script

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -12,6 +12,10 @@ rm /tmp/.auth
 
 echo "$DOCKER_PASS" > docker login -u $DOCKER_USER --password-stdin
 
-cd ~/pshs && docker compose up -d
+# In compose to mount volume will occur permission issue, so mkdir dir and touch file here.
+cd ~/pshs \
+&& mkdir -p results/check \
+&& touch debug.log \
+&& docker compose up -d
 
 exit 0
--- run.sh
+++ run.sh
@@ -50,4 +50,24 @@ if [ "$DEPLOY_PLACE" = "local" ];
         deploy/deploy.sh $DEPLOY_PLACE
 fi
 
+# deliver history data
+echo "**********************************"
+echo "** Deliver history data***********"
+echo "**********************************"
+
+# scp -i $SSH_PEM ./results/20211001_F_lvr_land_B.csv $DEPLOY_PLACE:~/pshs/results/20211001_F_lvr_land_B.csv
+# scp -i $SSH_PEM ./results/20220101_F_lvr_land_B.csv $DEPLOY_PLACE:~/pshs/results/20220101_F_lvr_land_B.csv
+# scp -i $SSH_PEM ./results/20220401_F_lvr_land_B.csv $DEPLOY_PLACE:~/pshs/results/20220401_F_lvr_land_B.csv
+# scp -i $SSH_PEM ./results/20220601_F_lvr_land_B.csv $DEPLOY_PLACE:~/pshs/results/20220601_F_lvr_land_B.csv
+# scp -i $SSH_PEM ./results/20220701_F_lvr_land_B.csv $DEPLOY_PLACE:~/pshs/results/20220701_F_lvr_land_B.csv
+# scp -i $SSH_PEM ./results/20220711_F_lvr_land_B.csv $DEPLOY_PLACE:~/pshs/results/20220711_F_lvr_land_B.csv
+# scp -i $SSH_PEM ./results/20220721_F_lvr_land_B.csv $DEPLOY_PLACE:~/pshs/results/20220721_F_lvr_land_B.csv
+# scp -i $SSH_PEM ./results/20220801_F_lvr_land_B.csv $DEPLOY_PLACE:~/pshs/results/20220801_F_lvr_land_B.csv
+# scp -i $SSH_PEM ./results/20220811_F_lvr_land_B.csv $DEPLOY_PLACE:~/pshs/results/20220811_F_lvr_land_B.csv
+# scp -i $SSH_PEM ./results/20220821_F_lvr_land_B.csv $DEPLOY_PLACE:~/pshs/results/20220821_F_lvr_land_B.csv
+# scp -i $SSH_PEM ./results/20220901_F_lvr_land_B.csv $DEPLOY_PLACE:~/pshs/results/20220901_F_lvr_land_B.csv
+# scp -i $SSH_PEM ./results/20220911_F_lvr_land_B.csv $DEPLOY_PLACE:~/pshs/results/20220911_F_lvr_land_B.csv
+
+# truncate database and insert history data!!!!
+ssh -i $SSH_PEM $DEPLOY_PLACE "docker exec --workdir /home/admin schedule venv/bin/python3 app.py init"
 exit 0
