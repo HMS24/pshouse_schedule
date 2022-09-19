211bfe582c8c66bf05ab2883c6f5ac2a98921b9f
chenrohua24 <z7707092003@gmail.com>
2022-09-19 15:41:44 +0800

    fix: database connection idle

--- pshouse_schedule/config.py
+++ pshouse_schedule/config.py
@@ -8,6 +8,8 @@ dotenv_path = os.path.join(cwd, ".env")
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
+SECONDS_OF_ONE_HOUR = 3600
+
 STORAGE_ROOT_DIR = "./results"
 STORAGE_BUCKET_NAME = "pshouse-schedule"
 STORAGE_TYPE = os.getenv("STORAGE_TYPE") or "LOCAL"
@@ -17,7 +19,9 @@ STORAGE_SECRET = os.getenv("STORAGE_SECRET") or "test-secret"
 SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI") or \
     "sqlite:///" + os.path.join(basedir, "app.db")
 SQLALCHEMY_ENGINE_OPTIONS = {
-    "echo": os.getenv("SQL_ECHO", "False") == "True"
+    "echo": os.getenv("SQL_ECHO", "False") == "True",
+    # immediately fix: MySQL Server has gone away
+    "pool_recycle": SECONDS_OF_ONE_HOUR,
 }
 
 SCHEDULER_TIMEZONE = "Asia/Taipei"
